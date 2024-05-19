25015ca4b6f8fcbcbb905cd1e08bd0700f18b5b3
Pavel Miatlitski <p.metlitsky@itransition.com>
2024-05-19 22:00:29 +0200

    feat: 2 implementations for modal pop up

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import Modal from './Modal';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [open, setOpen] = useState(false);
 
+  const modalBtnClickHandler = () => {
+    setOpen(true);
+  };
+
+  const closeModalHandler = () => {
+    setOpen(false);
+  };
+
+  // implementation WITHOUT animation
+
+  // return (
+  //   <div className="container">
+  //     <div className="App">
+  //       <button
+  //         className="open-modal-btn"
+  //         onClick={() => {
+  //           modalBtnClickHandler();
+  //         }}
+  //       >
+  //         ✨ Открыть окно
+  //       </button>
+  //       {open ? <Modal clickHandler={closeModalHandler} /> : null}
+  //     </div>
+  //   </div>
+  // );
+
+  // implementation WITH animation
+  const modalClassName = `animated ${open ? 'show' : ''}`;
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className="container">
+      <div className="App">
+        <button
+          className="open-modal-btn"
+          onClick={() => {
+            modalBtnClickHandler();
+          }}
+        >
+          ✨ Открыть окно
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <Modal
+          clickHandler={closeModalHandler}
+          className={modalClassName}
+          title="this is modal pop up"
+        >
+          <img
+            src="https://c.tenor.com/AG66DscMTg0AAAAd/tenor.gif"
+            alt="Robin Williams speaking wazzuuuuup!"
+          />
+        </Modal>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
+
+  //  how to close by clicking outside the window
+  // https://www.dhiwise.com/post/the-ultimate-guide-to-react-click-outside-modal-to-close
+
+  // how to close modal by pressing ESC key
+  // https://keyholesoftware.com/cancel-a-react-modal-with-escape-key-or-external-click/
 }
 
-export default App
+export default App;
