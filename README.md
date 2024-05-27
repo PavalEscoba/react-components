9b699ea1ab4b559c5b20814ef16e9ed39bdcd7c8
Pavel Miatlitski <p.metlitsky@itransition.com>
2024-05-27 18:07:27 +0200

    feat: implements photos app with categories and pagination

--- README.md
+++ README.md
@@ -1,8 +1,115 @@
-# React + Vite
+# Photos
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+Делаем запрос сюда: https://664f76b6ec9b4a4a602ee269.mockapi.io/api/photo-collections
 
-Currently, two official plugins are available:
+Сохраняем в стейт.
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+Маппим по нему и передаём в Collection - name и массив фоток.
+
+Делаем контролируемый инпут seachInputValue.
+И реализуем поиск по названиям. collections.filter...
+
+Реализуем фильтрацию по категориям. Они забиты номерами.
+Сначала реализуем рендер всех категорий. Они захардкожены. И выбор реализовывается через соответствие categoryId === index.
+
+```
+  <li
+    onClick={() => setCategoryId(i)}
+    key={cat.name}
+    className={`${categoryId === i ? 'active' : ''}`}
+  >
+    {cat.name}
+  </li>
+```
+
+Делаем фильтеринг на бэке. Добавляем URL параметр `?category=2`.
+
+https://664f76b6ec9b4a4a602ee269.mockapi.io/api/photo-collections?category=2
+
+```
+  const [categoryId, setCategoryId] = useState(0);
+```
+
+```
+  useEffect(() => {
+    fetch(
+      `https://664f76b6ec9b4a4a602ee269.mockapi.io/api/photo-collections${categorySlug}`
+    )
+      .then((res) => res.json())
+      .then((json) => setCollections(json))
+      .catch((err) => {
+        console.error('Error during fetching from database: ', err);
+      });
+  }, [categoryId]);
+```
+
+Вводим функционал isLoading.
+
+```
+  const [isLoading, setIsLoading] = useState(true);
+```
+
+```
+useEffect(() => {
+  setIsLoading(true);
+  fetch(
+    `https://664f76b6ec9b4a4a602ee269.mockapi.io/api/photo-collections${categorySlug}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      setCollections(json);
+    })
+    .catch((err) => {
+      console.error('Error during fetching from database: ', err);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    });
+}, [categoryId]);
+```
+
+Делаем пагинацию.
+
+```
+const [page, setPage] = useState(1);
+```
+
+```
+<ul className="pagination">
+  {[...Array(5)].map((_, i) => {
+    return (
+      <li
+        key={`page${i}`}
+        onClick={() => {
+          setPage(i + 1);
+        }}
+        className={page === i + 1 ? 'active' : ''}
+      >
+        {i + 1}
+      </li>
+    );
+  })}
+</ul>
+```
+
+```
+const categorySlug = categoryId ? `&category=${categoryId}` : '';
+const pageSlug = page ? `&page=${page}` : ``;
+
+useEffect(() => {
+  setIsLoading(true);
+  fetch(
+    `https://664f76b6ec9b4a4a602ee269.mockapi.io/api/photo-collections?limit=2&${categorySlug}&${pageSlug}`
+  )
+    .then((res) => res.json())
+    .then((json) => {
+      setCollections(json);
+    })
+    .catch((err) => {
+      console.error('Error during fetching from database: ', err);
+    })
+    .finally(() => {
+      setIsLoading(false);
+    });
+}, [categoryId, page]);
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,118 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Collection from './components/Collection';
 import './App.scss';
 
+const cats = [
+  { name: 'Все' },
+  { name: 'Море' },
+  { name: 'Горы' },
+  { name: 'Архитектура' },
+  { name: 'Города' },
+];
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [categoryId, setCategoryId] = useState(0);
+  const [page, setPage] = useState(1);
+  const [collections, setCollections] = useState([]);
+  const [seachInputValue, setSeachInputValue] = useState('');
+
+  const categorySlug = categoryId ? `&category=${categoryId}` : '';
+  const pageSlug = page ? `&page=${page}` : ``;
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://664f76b6ec9b4a4a602ee269.mockapi.io/api/photo-collections?limit=2&${categorySlug}&${pageSlug}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.error('Error during fetching from database: ', err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, [categoryId, page]);
+
+  const collectionsNames = () => {
+    return (
+      <ul>
+        {collections.map((collection, i) => {
+          return <li key={(collection.name, i)}> {collection.name} </li>;
+        })}
+      </ul>
+    );
+  };
+
+  const filteredPhotoColections = collections.filter((collection) => {
+    const collectionNameLowerCase = collection.name.toLowerCase();
+    const searchQueryLowerCase = seachInputValue.toLowerCase();
+
+    return collectionNameLowerCase.includes(searchQueryLowerCase);
+  });
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+    <div className="App">
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {cats.map((cat, i) => {
+            return (
+              <li
+                onClick={() => setCategoryId(i)}
+                key={cat.name}
+                className={`${categoryId === i ? 'active' : ''}`}
+              >
+                {cat.name}
+              </li>
+            );
+          })}
+        </ul>
+
+        <input
+          className="search-input"
+          placeholder="Поиск по названию"
+          value={seachInputValue}
+          onChange={(evt) => {
+            setSeachInputValue(evt.target.value);
+          }}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      {/* <div>{collectionsNames()}</div> */}
+      <ul className="pagination">
+        {[...Array(5)].map((_, i) => {
+          return (
+            <li
+              key={`page${i}`}
+              onClick={() => {
+                setPage(i + 1);
+              }}
+              className={page === i + 1 ? 'active' : ''}
+            >
+              {i + 1}
+            </li>
+          );
+        })}
+      </ul>
+      <div className="content">
+        {isLoading ? (
+          <p>Loading ...</p>
+        ) : (
+          filteredPhotoColections.map((collection, i) => {
+            return (
+              <Collection
+                key={collection.name + i}
+                name={collection.name}
+                images={collection.photos}
+              />
+            );
+          })
+        )}
+      </div>
+    </div>
   );
 }
 
